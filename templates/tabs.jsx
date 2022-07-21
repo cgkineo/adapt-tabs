43962388528c423c07b64a9f1f258e8efe72082d
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2022-07-21 16:26:07 +0100

    Unneccesary curly braces removed.

--- templates/tabs.jsx
+++ templates/tabs.jsx
@@ -38,7 +38,7 @@ export default function Tabs(props) {
                 aria-controls={`${_id}-${index}-tabpanel`}
                 aria-hidden={!_isActive || null}
                 aria-label={`${tabTitle}.${_isVisited ? ` ${_globals._accessibility._ariaLabels.visited}` : ''}`}
-                tabIndex={`${_isActive ? 0 : -1}`}
+                tabIndex={_isActive ? 0 : -1}
                 className={classes([
                   'tabs__nav-item-btn',
                   'js-tabs-nav-item-btn-click',
@@ -75,7 +75,7 @@ export default function Tabs(props) {
                 role="tabpanel"
                 aria-hidden={!_isActive || null}
                 aria-labelledby={`${_id}-${index}-tabtitle`}
-                tabIndex={`${_isActive ? 0 : -1}`}
+                tabIndex={_isActive ? 0 : -1}
                 data-index={_index}
                 className={classes([
                   'tabs__content-item',
